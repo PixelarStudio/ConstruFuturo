001679b6fa9fded9248681feb4824807742893d9
Cristian Ruiz <cjruiz2013@gmail.com>
2024-04-25 14:23:26 -0300

    Se mejoran las rutas de los componentes, y se modifico el main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react(), mdx(), sitemap(), partytown()]
 });
